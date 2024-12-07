59c25474d5661536d3d131c168e7b6b47f1d6e9c
atikur-24 <atikurrahman7223@gmail.com>
2024-12-07 23:57:07 +0600

    design profile header and show info

---
+++ src/app/dashboard/profile/_components/ProfileHeader.jsx
@@ -0,0 +1,39 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const ProfileHeader = ({
+  image,
+  fullName,
+  userId,
+  walletAddress,
+  referredBy,
+  joiningDate,
+}) => {
+  return (
+    <div className="dashboard-bottom-spacing">
+      <div className="space-y-4 rounded-lg bg-arx-black-4 p-5 text-center shadow-lg md:space-y-5 md:p-6">
+        {/* edit profile part*/}
+        <div>
+          <Avatar className="mx-auto h-16 w-16 md:h-20 md:w-20">
+            <AvatarImage src={image || "/images/dashboard/avatar.png"} />
+            <AvatarFallback>User</AvatarFallback>
+          </Avatar>
+          <h1 className="mb-2 text-xl font-bold md:text-2xl">{fullName}</h1>
+          <Button size="sm" variant="secondary">
+            Edit Profile
+          </Button>
+        </div>
+        {/* profile info part */}
+        <div className="text-sm text-zinc-300">
+          <p>ID: {userId}</p>
+          <p>Invited By: {referredBy}</p>
+          <p>Wallet: {walletAddress}</p>
+          <p>Joined: {joiningDate}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileHeader;
