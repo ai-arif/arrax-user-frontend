816f100c6e4b2574c5e168d7e077c04efc5b393a
atikur-24 <atikurrahman7223@gmail.com>
2024-12-10 15:26:56 +0600

    apply animation in home stats section

--- src/app/(main)/_components/HomeStats.jsx
+++ src/app/(main)/_components/HomeStats.jsx
@@ -1,17 +1,31 @@
+"use client";
+
 import Container from "@/components/ui/Container";
 import SectionTitle from "@/components/ui/SectionTitle";
-import React from "react";
+import "animate.css";
+import React, { useEffect } from "react";
+const WOW = typeof window !== "undefined" ? require("wowjs") : null;
 
 const HomeStats = () => {
   const totalUsersToday = 123;
   const totalIncomeToday = 4567.89;
+
+  useEffect(() => {
+    if (WOW) {
+      new WOW.WOW().init();
+    }
+  }, []);
 
   return (
     <Container>
       <SectionTitle>Today&apos;s Summary</SectionTitle>
       <div className="grid grid-cols-1 gap-4 text-center md:grid-cols-2 md:gap-8">
         {/* Total Users Today */}
-        <div className="rounded border border-arx-secondary p-5 md:rounded-lg md:p-6">
+        <div
+          className="wow animate__animated animate__fadeInUp rounded border border-arx-secondary p-5 md:rounded-lg md:p-6"
+          data-wow-delay="0.2s"
+          data-wow-duration="1s"
+        >
           <h3 className="mb-2 text-base font-medium md:text-lg md:font-semibold">
             Total Users (Today)
           </h3>
@@ -20,7 +34,11 @@ const HomeStats = () => {
           </p>
         </div>
         {/* Total Income Today */}
-        <div className="rounded border border-arx-secondary p-5 md:rounded-lg md:p-6">
+        <div
+          className="wow animate__animated animate__fadeInUp rounded border border-arx-secondary p-5 md:rounded-lg md:p-6"
+          data-wow-delay="0.2s"
+          data-wow-duration="1s"
+        >
           <h3 className="mb-2 text-base font-medium md:text-lg md:font-semibold">
             Total Income (Today)
           </h3>
