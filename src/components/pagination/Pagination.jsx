8c6048b6d53b3735187a0dfcf619e5f63dbdc1d2
atikur-24 <atikurrahman7223@gmail.com>
2025-01-12 22:25:33 +0600

    improved pagination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 import ReactPaginate from "react-paginate";
 
@@ -5,21 +6,31 @@ const Pagination = ({ currentPage, totalPages, handlePageChange }) => {
   return (
     <div className="mt-5">
       <ReactPaginate
-        className="flex flex-wrap items-center justify-center gap-3 pb-5 text-center text-sm font-semibold text-slate-300"
-        activeClassName="bg-purple-600 text-white border border-purple-600"
-        previousClassName={`px-3 py-1.5 rounded border ${currentPage === 1 ? "cursor-not-allowed opacity-50" : "hover:bg-purple-600 hover:text-white hover:border-purple-600 transition-all"}`}
-        nextClassName={`rounded border ${currentPage === totalPages ? "cursor-not-allowed opacity-50" : "hover:bg-purple-600 hover:text-white hover:border-purple-600 transition-all"}`}
-        disabledClassName="cursor-not-allowed opacity-50"
-        pageClassName="border rounded "
-        pageLinkClassName="block px-3 py-1.5 hover:bg-purple-600 hover:border-purple-600 hover:text-white hover:border-purple-600 transition-all text-center w-full"
-        nextLinkClassName="block px-3 py-1.5 hover:bg-purple-600 hover:text-white hover:border-purple-600 transition-all text-center w-full"
+        className="flex flex-wrap items-center justify-center gap-3 pb-5 text-sm font-semibold text-zinc-300"
+        activeLinkClassName={clsx(
+          "bg-purple-600 text-white border-purple-600",
+          "hover:bg-purple-800",
+        )}
+        pageLinkClassName="border rounded px-3 py-1.5 hover:bg-zinc-800"
+        previousLinkClassName={clsx(
+          "px-3 py-1.5 rounded border",
+          currentPage === 1
+            ? "cursor-not-allowed opacity-50"
+            : "hover:bg-zinc-800",
+        )}
+        nextLinkClassName={clsx(
+          "px-3 py-1.5 rounded border",
+          currentPage === totalPages
+            ? "cursor-not-allowed opacity-50"
+            : "hover:bg-zinc-800",
+        )}
         breakClassName="px-2 py-1"
-        breakLabel="..."
+        previousLabel="<"
         nextLabel=">"
+        breakLabel="..."
         onPageChange={handlePageChange}
         pageRangeDisplayed={5}
         pageCount={totalPages || 0}
-        previousLabel="<"
         renderOnZeroPageCount={null}
       />
     </div>
